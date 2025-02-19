55fcd6cd1240141db27bc9de8ea6236684ee5dd7
AllsCastro <allisoncastro0515@gmail.com>
2025-02-18 22:34:02 -0600

    EvaluacionAllison.java

--- EvaluacionAllison.java
+++ EvaluacionAllison.java
@@ -2,19 +2,42 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package evaluacion.allison;
-
+package evaluacion.pkg1.allison.arias.castro;
+import javax.swing.JOptionPane;
 /**
  *
- * @author allis
+ * @author Allison
  */
-public class EvaluacionAllison {
+public class Evaluacion1AllisonAriasCastro {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String empleadoespecifico = ""; // Declaramos la variable del nombre
+        double salarioMensual = 0; //Salario bruto
+        double salarioMensualDeduccionesSEM= 0; //Salario neto
+        double salarioMensualDeduccionesIVM = 0; //Salario neto
+        String variableTemporal="";
+        
+        empleadoespecifico= JOptionPane.showInputDialog("Digite su nombre");
+        variableTemporal= JOptionPane.showInputDialog("Digite su salario");
+        
+      
+        salarioMensual=Double.parseDouble(variableTemporal);
+        
+        salarioMensualDeduccionesSEM=salarioMensual*0.0925;
+        salarioMensualDeduccionesIVM=salarioMensual*0.0508;
+        
+        JOptionPane.showMessageDialog(null, "El nombre es " + empleadoespecifico + "\n" +
+                                            "El salario mensual (salario bruto)es "+ salarioMensual + "\n" +
+                                            "El salario mensual con deducciones SEM (salario neto)es "+ salarioMensualDeduccionesSEM + "\n" +
+                                            "El salario mensual con deducciones IVM (salario neto)es "+ salarioMensualDeduccionesIVM + "\n");
+
+        
+        
     }
     
 }
